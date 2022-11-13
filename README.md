02277896b48a25f9c61eb9dd3b26870f6ce9b262
tejassurajkoundinya@gmail.com <77600063+TEJASsKoundinya@users.noreply.github.com>
2022-11-13 20:48:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pitch_Mask
+ Identify the pitch area
---
+++ Run.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from os import listdir
+from os.path import isfile, join
+from PIL import Image
+
+
+
+onlyfiles = [f for f in listdir("Images_test_set")
+             if isfile(join("Images_test_set", f))]
+
+print(onlyfiles)
+
+destination0 = 'Output_BW_test_set/'
+destination1 = 'Mask/'
+for i in onlyfiles:
+   path = 'Images_test_set/'+i
+   img = cv2.imread(path)
+   print(img.shape)
+   gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   (thresh, binary) = cv2.threshold(gray_image, 150, 225, cv2.THRESH_BINARY)
+   cv2.imwrite(destination0+i, binary)
+   mask = cv2.imread(destination0+i, 0)
+   mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+   img[mask == 255] = (36, 255, 12)
+   cv2.imwrite(destination1+i, img)
+
+   cv2.imshow('image', img)
+   #cv2.imshow('mask', mask)
+   
+
+
+   ##########BOX##########
+   #cv2.rectangle(image, start_point, end_point, color, thickness)
+   #cv2.rectangle(Face, (x, y), (x+w, y+h), (255, 255, 255), thickness)
+   cv2.waitKey(1000)
+
+   
+###################################################333
+   #img1 = cv2.cvtColor(binary,cv2.COLOR_BGR2RGB)
+   #hsv = cv2.cvtColor(img1, cv2.COLOR_RGB2HSV)
+   # define range of brown color in HSV
+   #lower_brown = np.array([0, 0, 255])
+   #upper_brown = np.array([255, 255, 255])
+   # Threshold the HSV image to get only blue colors
+   #mask = cv2.inRange(hsv, lower_brown, upper_brown)
+   # Bitwise-AND mask and original image
+   #patch = cv2.bitwise_and(img1, img1, mask=mask)
+   #cv2.waitKey(1000)
+   #cv2.imshow('Patch of pitch', patch)
+   #cv2.imwrite(destination1+i, patch)
